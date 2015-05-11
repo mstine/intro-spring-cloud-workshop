f98cbcb08b5caa8200d8d70168bc600adc7db8a3
Matt Stine <mstine@gopivotal.com>
2015-05-11 12:42:10 -0700

    missing eureka dependency and @EnableDiscoveryClient

--- cloud/complete/spring-cloud-labs/zuul/src/main/java/io/pivotal/springcloud/zuul/ZuulApplication.java
+++ cloud/complete/spring-cloud-labs/zuul/src/main/java/io/pivotal/springcloud/zuul/ZuulApplication.java
@@ -2,10 +2,12 @@ package io.pivotal.springcloud.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class ZuulApplication {
 
     public static void main(String[] args) {
