5ea7ed8e15cdf64f4ce8204a30fb8246957ce595
Matt Stine <mstine@gopivotal.com>
2015-05-12 10:45:28 -0700

    add eureka registration

--- cloud/complete/spring-cloud-labs/dist-config/src/main/java/io/pivotal/springcloud/distconfig/DistConfigApplication.java
+++ cloud/complete/spring-cloud-labs/dist-config/src/main/java/io/pivotal/springcloud/distconfig/DistConfigApplication.java
@@ -3,11 +3,13 @@ package io.pivotal.springcloud.distconfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class DistConfigApplication {
 
     @Autowired
